01058409fd9bedc4817a4dbe930999cda8e0028e
Ivan <ivan.dziashchenia@belcommunmash.by>
2024-08-20 15:53:58 +0300

    fix responsive

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function menuWorker() {
+    let element = document.getElementById("burger_menu");
+    let buttonImage = document.getElementById("burger_button");
+
+    if (element.style.top == "-100%") {
+        element.style.top = "0";
+        buttonImage.src = "assets/icons/close.svg";
+    } else {
+        element.style.top = "-100%";
+        buttonImage.src = "assets/icons/burger.svg";
+    }
+}
